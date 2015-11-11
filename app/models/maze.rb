78e66a7ba16c85dd573182da4457e586c9b03069
Eaden <_@eaden.net>
2015-11-11 14:15:09 +0000

    initial graph generation algo

--- app/models/maze.rb
+++ app/models/maze.rb
@@ -1,7 +1,27 @@
 class Maze
-  attr :graph
+  attr :graph, :exit_id
 
   def initialize
     @graph = Graph.new
+  end
+
+  def generate(size = 30)
+    @exit_id = size - 1 # the last vertex created
+    level_verticies = [:root]
+    new_level = []
+    vertexID = 0
+    while vertexID < size
+      level_verticies.each do |vertex|
+        new_level = []
+        forks = rand(4)+1
+        forks.times do
+          graph.add_vertex(vertexID)
+          graph.add_edge(from: vertex, to: vertexID)
+          new_level << vertexID
+          vertexID += 1
+        end
+      end
+      level_verticies = new_level
+    end
   end
 end
--- spec/models/maze_spec.rb
+++ spec/models/maze_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Maze do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:maze) { described_class.new }
+  describe "#generate" do
+    it "creates a graph with at least a 2-way fork at the start" do
+      maze.generate
+      graph = maze.graph
+      expect(graph.vertex(:root).edges.size).to be > 1
+    end
+  end
 end
