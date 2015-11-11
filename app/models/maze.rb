eade10a855f9733144bdf4e20789fa4a60963b9e
Eaden <_@eaden.net>
2015-11-11 14:22:08 +0000

    test the exit id

--- app/models/maze.rb
+++ app/models/maze.rb
@@ -1,19 +1,22 @@
 class Maze
-  attr :graph, :exit_id
+  attr :graph
 
-  def initialize
+  def initialize(size = 30)
     @graph = Graph.new
+    @size = size
   end
 
-  def generate(size = 30)
-    @exit_id = size - 1 # the last vertex created
+  def exit_id
+    @size - 1 # the last vertex to be generated
+  end
+
+  def generate
     level_verticies = [:root]
     new_level = []
     vertexID = 0
-    while vertexID < size
+    while vertexID < @size
       level_verticies.each do |vertex|
         new_level = []
-        forks = rand(4)+1
         forks.times do
           graph.add_vertex(vertexID)
           graph.add_edge(from: vertex, to: vertexID)
@@ -23,5 +26,12 @@ class Maze
       end
       level_verticies = new_level
     end
+  end
+
+  private
+
+  # the number of forks at each level
+  def forks
+    rand(2)+2 # (2-4)
   end
 end
--- spec/models/maze_spec.rb
+++ spec/models/maze_spec.rb
@@ -9,4 +9,13 @@ RSpec.describe Maze do
       expect(graph.vertex(:root).edges.size).to be > 1
     end
   end
+
+  describe "#exit_id" do
+    it "gives the id of the exit node" do
+      maze.generate
+      exit_id = maze.exit_id
+      expect(exit_id).to_not be_nil
+      expect(maze.graph.vertex(exit_id)).to_not be_nil
+    end
+  end
 end
